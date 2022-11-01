06a8496f7563f5cfdf55a7347bb32d7bcbd2791a
dasun <dasun@wso2.com>
2022-11-01 12:40:54 +0530

    setting port

--- byoc-test/app.js
+++ byoc-test/app.js
@@ -1,20 +1,18 @@
 const express = require('express')
 const morgan = require('morgan')
-
-
-
-
-
 const app = express();
+
+
+
 app.use(morgan('tiny'));
 app.use(express.json())
 
 
-
+const PORT  = process.env.PORT || 8080
 const moviesRoute = require('./routes/movies')
 
 
 app.use("/movies", moviesRoute)
 
 
-app.listen(9090, () => console.log("Server is running on port 9090"))
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
